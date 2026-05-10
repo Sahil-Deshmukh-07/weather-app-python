95c238ebdc535987ec5f24fed414b5e98f7acb31
Sahil-Deshmukh-07 <sahildeshmukh9511@gmail.com>
2026-05-10 15:20:59 +0530

    Update weather.py

--- weather.py
+++ weather.py
@@ -4,7 +4,6 @@ API_KEY = "YOUR_API_KEY"
 Base_URL = "http://api.openweathermap.org/data/2.5/weather"
 result = ""
 
-# city = input("Enter city name: ")
 def get_weather(city:str):
 
     params = {
@@ -19,11 +18,9 @@ def get_weather(city:str):
     if data["cod"] == 200:
         main = data["main"]
         weather = data["weather"][0]
-        # print(f"City: {city.capitalize()}\nTemp: {main['temp']}*C\nFeels like: {main['feels_like']}*C\nHumidity: {main['humidity']}\nWeather: {weather['description'].capitalize()}")
         result = f"City: {city.capitalize()}\nTemp: {main['temp']}*C\nFeels like: {main['feels_like']}*C\nHumidity: {main['humidity']}\nWeather: {weather['description'].capitalize()}"
 
     else:
-        # print("City not found")
         result = "City not found"
     
     return result
